6cbb8108eb5682f0f1a87f902dc60ccb16a89a30
Athulya <athulya.raghu@tcs.com>
2021-04-25 16:11:57 +0200

    Editing

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,7 +4,7 @@ insert into book (id , name , isbn ) VALUES (102,'abc','def');
 --create table users (id int auto_increment primary key, name varchar);
 insert into users (id , name ) VALUES (101,'abc');
 
-insert into register (id , boook_id, action, borrowedUser ) VALUES (101,102,"BORRWORED",101);
+insert into register (id , boook_id, action, borrowedUser ) VALUES (101,102,"BORROWED",101);
 
 --create table library (id int auto_increment primary key, bookId int not null, isBorrowed varchar, userId int);
 --insert into book (1,1,"abc");
